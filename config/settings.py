1125be27ee9773a4515cd390bcc40c7fc3d56d5a
douglas <hermelio.ifma@gmail.com>
2026-01-12 14:53:23 -0300

    Atualizar logo para logotipoTreinaCNH.png

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Site Configuration
 SITE_NAME = 'TREINACNH'
-SITE_LOGO = 'images/logo.png'  # Path relativo a STATIC_URL
+SITE_LOGO = 'images/logotipoTreinaCNH.png'  # Path relativo a STATIC_URL
 SITE_URL = config('SITE_URL', default='http://localhost:8000')
 
 # Default primary key field type
